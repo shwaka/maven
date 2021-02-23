eb8457367efdc55a5ef9bc2a0fb6cbdbcf80e1d9
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-02-23 12:55:14 +0900

    add git-set-file-times.sh

---
+++ git-set-file-times.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -eu
+
+for FILE in `git ls-files`; do
+  TIME=`git log --pretty=format:%ci -n1 $FILE`
+  echo "$TIME $FILE"
+  STAMP=`date -d "$TIME" +"%y%m%d%H%M.%S"`
+  touch -t $STAMP $FILE
+done
