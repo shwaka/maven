52e061fbaccf8070d0b3eebdb0d700a394ad80d8
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-02-27 13:30:22 +0900

    replace 'git ls-files' with 'find repository'

--- git-set-file-times.sh
+++ git-set-file-times.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-for FILE in `git ls-files`; do
+for FILE in `find repository`; do
   TIME=`git log --pretty=format:%ci -n1 $FILE`
   echo "$TIME $FILE"
   STAMP=`date -d "$TIME" +"%y%m%d%H%M.%S"`
